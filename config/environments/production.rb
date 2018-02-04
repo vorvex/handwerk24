579a6f6b64b03b117c1c2c8187f53baff3ac9e42
Oneline <leon.dahmer@gmail.com>
2018-02-04 16:00:34 -0500

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,9 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
   
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.serve_static_assets = false
-  config.assets.digest = true
-  config.assets.compile = true
+  config.assets.compile = false
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
