60be8e1db2ba64a89127c7e025fd6fdb1e84fddf
Oneline <leon.dahmer@gmail.com>
2018-02-15 17:16:54 -0500

    Fix Inquiery.rb

--- app/models/inquiery.rb
+++ app/models/inquiery.rb
@@ -5,7 +5,7 @@ class Inquiery < ApplicationRecord
   has_many :services, through: :service_inquieries
   before_save { self.email = email.downcase }
   validates :name, presence: true  
-  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
   #validates :adresse, presence: true
   validates :plz, presence: true
